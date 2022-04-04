e33a4b54025c4d29c9ef588116e8382bc15a009f
Karly3295 <103004570+Karly3295@users.noreply.github.com>
2022-04-04 18:15:42 -0500

    Add files via upload

---
+++ CS320P03Latkiewicz/input_Cpp01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// C++ programs start by executing the function main
+int main()
+{
+    // output "Hello, World!" followed by a new line \n
+    cout << "Hello, World!\n";
+
+    cin.get(); //keep window open
+    return 0; //return a value indicating success
+}
+
